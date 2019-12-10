81a77a9fc003e311365647241ef49266f3197a52
anna611 <a0961130611@gmail.com>
2019-12-10 17:51:32 +0800

    Add code

--- utils.py
+++ utils.py
@@ -9,9 +9,10 @@ channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
 
 def send_text_message(reply_token, text):
     line_bot_api = LineBotApi(channel_access_token)
-    line_bot_api.reply_message(reply_token,text)
+    line_bot_api.reply_message(reply_token, TextSendMessage(text=text))
 
     return "OK"
+
 
 """
 def send_image_url(id, img_url):
