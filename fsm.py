bedd3f02dd34c8dc657cb3d6af059aa6bbd3c9ac
anna611 <a0961130611@gmail.com>
2019-12-10 18:09:29 +0800

    Add code

--- fsm.py
+++ fsm.py
@@ -1,7 +1,7 @@
 from transitions.extensions import GraphMachine
 
 from utils import send_text_message
-
+from linebot.models import *
 
 class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
